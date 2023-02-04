06b9531c703fd21e309bde90faa32406ffcb4c38
Joel-dev-IMP <77357583+Joel-dev-IMP@users.noreply.github.com>
2023-02-04 17:30:49 +0100

    create-project.js: Kommentare ergänzt

--- create-project.js
+++ create-project.js
@@ -1,11 +1,14 @@
-const setValue = (t) => {
-    if (Number(t.value) < Number(t.min)) {
-        t.value = t.min;
+// Funktion, um bei Über- oder Unterschreitung der Grenzwerte eines number-inputs
+// automatisch den entsprechenden Grenzwert als Wert einzusetzen.
+// Bsp: min=5, max=10; Eingegebener Wert: 42 -> 10 wird als neuer Wert des inputs genommen.
+const setValue = (target) => {
+    if (Number(target.value) < Number(target.min)) {
+        target.value = target.min;
         return;
     }
 
-    if (Number(t.value) > Number(t.max)) {
-        t.value = t.max;
+    if (Number(target.value) > Number(target.max)) {
+        target.value = target.max;
         return;
     }
-}
+};
