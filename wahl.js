63e4c38df24cd5eb86d30d3d6700466074c7a4c5
Joel-dev-IMP <77357583+Joel-dev-IMP@users.noreply.github.com>
2023-02-04 17:41:40 +0100

    wahl.js: Indentation von 4 auf 2 Leerzeichen geändert

--- wahl.js
+++ wahl.js
@@ -1,100 +1,100 @@
 function getSelectedProjects() {
-    let selectedProjects = [];
-    document.querySelectorAll(".drag-container>div").forEach((selected) => {
-        selectedProjects.push(selected.dataset.value);
-    });
+  let selectedProjects = [];
+  document.querySelectorAll(".drag-container>div").forEach((selected) => {
+    selectedProjects.push(selected.dataset.value);
+  });
 
-    return selectedProjects;
+  return selectedProjects;
 }
 
 function handleSelect({ target }) {
-    const projectId = target.dataset.value;
-    const projectName =
-        target.parentElement.parentElement.parentElement.children[0].innerText;
+  const projectId = target.dataset.value;
+  const projectName =
+    target.parentElement.parentElement.parentElement.children[0].innerText;
 
-    const container = document.querySelector(".drag-container");
+  const container = document.querySelector(".drag-container");
 
-    const selectedProjects = getSelectedProjects();
-    console.log(selectedProjects);
-    console.log(target);
+  const selectedProjects = getSelectedProjects();
+  console.log(selectedProjects);
+  console.log(target);
 
-    // Projekt entfernen
-    if (selectedProjects.includes(projectId)) {
-        // Projekt, das entfernt werden soll, aus der Drag-n-drop-Liste auswählen und entfernen.
-        const removedProject = container.querySelector(
-            `div[data-value="${projectId}"]`
-        );
-        container.removeChild(removedProject);
+  // Projekt entfernen
+  if (selectedProjects.includes(projectId)) {
+    // Projekt, das entfernt werden soll, aus der Drag-n-drop-Liste auswählen und entfernen.
+    const removedProject = container.querySelector(
+      `div[data-value="${projectId}"]`
+    );
+    container.removeChild(removedProject);
 
-        container.style.height = 6 * (selectedProjects.length - 1) + "rem";
+    container.style.height = 6 * (selectedProjects.length - 1) + "rem";
 
-        // Projekt in der Liste als "Nicht ausgewählt" markieren.
-        target.parentElement.parentElement.parentElement.classList.remove(
-            "selected"
-        );
-        target.innerText = "Auswählen";
+    // Projekt in der Liste als "Nicht ausgewählt" markieren.
+    target.parentElement.parentElement.parentElement.classList.remove(
+      "selected"
+    );
+    target.innerText = "Auswählen";
 
-        return;
-    }
+    return;
+  }
 
-    if (selectedProjects.length >= 5) {
-        // Es dürfen nur fünf Projekte gewählt werden.
-        return;
-    }
+  if (selectedProjects.length >= 5) {
+    // Es dürfen nur fünf Projekte gewählt werden.
+    return;
+  }
 
-    // Add the project to the selected projects list.
-    target.parentElement.parentElement.parentElement.classList.add("selected");
-    target.innerText = "Auswahl aufheben";
+  // Add the project to the selected projects list.
+  target.parentElement.parentElement.parentElement.classList.add("selected");
+  target.innerText = "Auswahl aufheben";
 
-    container.style.height = 6 * (selectedProjects.length + 1) + "rem";
+  container.style.height = 6 * (selectedProjects.length + 1) + "rem";
 
-    addProject(projectId, projectName);
-    console.log(selectedProjects);
+  addProject(projectId, projectName);
+  console.log(selectedProjects);
 }
 
 function addProject(projectId, projectName) {
-    const container = document.querySelector(".drag-container");
-    const index = container.children.length + 1;
+  const container = document.querySelector(".drag-container");
+  const index = container.children.length + 1;
 
-    const draggable = document.createElement("div");
-    draggable.classList.add("draggable");
-    draggable.dataset.index = index;
-    draggable.dataset.value = projectId;
-    draggable.draggable = false;
-    draggable.innerText = projectName;
+  const draggable = document.createElement("div");
+  draggable.classList.add("draggable");
+  draggable.dataset.index = index;
+  draggable.dataset.value = projectId;
+  draggable.draggable = false;
+  draggable.innerText = projectName;
 
-    container.appendChild(draggable);
+  container.appendChild(draggable);
 }
 
 function validateSubmit() {
-    const selectedProjects = getSelectedProjects();
-    console.log(selectedProjects);
+  const selectedProjects = getSelectedProjects();
+  console.log(selectedProjects);
 
-    if (selectedProjects.length !== 5) {
-        return false;
-    }
+  if (selectedProjects.length !== 5) {
+    return false;
+  }
 
-    const container = document.querySelector(".drag-container");
+  const container = document.querySelector(".drag-container");
 
-    // Sicherstellen, dass ansonsten keine Inputs im Formular sind (keine Daten doppelt/verfälscht geschickt werden)
-    let inputs = container.parentElement.getElementsByTagName("input");
-    while (inputs.length > 0) {
-        container.removeChild(inputs[0]);
-    }
+  // Sicherstellen, dass ansonsten keine Inputs im Formular sind (keine Daten doppelt/verfälscht geschickt werden)
+  let inputs = container.parentElement.getElementsByTagName("input");
+  while (inputs.length > 0) {
+    container.removeChild(inputs[0]);
+  }
 
-    selectedProjects.forEach((projectId, index) => {
-        const input = document.createElement("input");
-        input.value = projectId;
-        input.type = "hidden";
-        input.name = `Wahl${index + 1}`;
+  selectedProjects.forEach((projectId, index) => {
+    const input = document.createElement("input");
+    input.value = projectId;
+    input.type = "hidden";
+    input.name = `Wahl${index + 1}`;
 
-        container.appendChild(input);
-    });
+    container.appendChild(input);
+  });
 
-    return true;
+  return true;
 }
 
 console.log(document.querySelectorAll(".select-button"));
 document.querySelectorAll(".select-button").forEach((button) => {
-    button.addEventListener("click", handleSelect);
+  button.addEventListener("click", handleSelect);
 });
